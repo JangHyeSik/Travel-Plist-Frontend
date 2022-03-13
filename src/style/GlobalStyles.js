8a7851d9a41529d46f43dd58f2d492594a29c689
JangHyeSik <hyesik9982@gmail.com>
2022-03-14 00:11:22 +0900

    chore: 핸드폰 레이아웃 스타일 수정

--- src/style/GlobalStyles.js
+++ src/style/GlobalStyles.js
@@ -14,7 +14,7 @@ const GlobalStyle = createGlobalStyle`
     width: 100%;
     min-width: 320px;
     margin-top: 5rem;
-    min-height: calc(100vh - 5rem);
+    min-height: calc(100vh - 15rem);
     font-family: Noto Sans KR, sans-serif;
     margin: 0;
     -ms-overflow-style: none;
